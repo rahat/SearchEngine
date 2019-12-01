8a56add64a2ffd660de84067953e91d85aaf1ca3
Rahat Mahmud <rahat.mahmud@macaulay.cuny.edu>
2019-11-30 23:53:32 -0500

    Implemented initial interface with JavaFX

---
+++ src/com/rahat/Controller.java
@@ -0,0 +1,14 @@
+package com.rahat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField query;
+
+    public void Search(ActionEvent actionEvent) {
+    }
+}
---
+++ src/com/rahat/Main.java
@@ -0,0 +1,22 @@
+package com.rahat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("SearchEngine.fxml"));
+        primaryStage.setTitle("Search Engine");
+        primaryStage.setScene(new Scene(root, 720, 480));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
